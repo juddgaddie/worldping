6379b00133b7e9c4c8be4eb0997256bfc3401180
Judd Gaddie <judd.gaddie@transficc.com>
2024-04-06 20:34:28 +0100

    Project setup

---
+++ app/src/test/java/net/gaddie/worldping/WorldpingTest.java
@@ -0,0 +1,12 @@
+package net.gaddie.worldping;
+
+import org.junit.jupiter.api.Test;
+
+public class WorldpingTest
+{
+    @Test
+    void name()
+    {
+        System.out.println("hello");
+    }
+}
